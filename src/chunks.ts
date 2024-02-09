d1b54ce283ebf8b8267a7101e0abc40278f40c56
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-08 17:40:45 -0800

    Chunk Parser: Chunks vs Entries
    Making this distinction again, similar to the concept with the Region parser. There's the data entries for the archive/save file itself, then there is the parsed-out structure of the chunks themselves. I don't want to have to require you to parse out all of the chunks at once just to get to a specific part, each stage of reading should be when you need to use it.

--- src/chunks.ts
+++ src/chunks.ts
@@ -2,9 +2,15 @@ export async function readChunksDat(data: Buffer): Promise<ChunksDat> {
   // console.log(data);
 
   console.log(data.subarray(0,SECTOR_LENGTH));
-  const chunkIndices = readChunkIndicies(data);
-  console.log(chunkIndices.slice(13,17));
+  const chunkEntries = readChunkEntries(data);
+  console.log(chunkEntries.slice(13,17));
 }
+
+export interface ChunksDat extends ReadonlyArray<Chunk | null> {
+  [index: number]: Chunk;
+}
+
+export interface Chunk {}
 
 export const SECTOR_LENGTH = 4096;
 
@@ -13,7 +19,7 @@ export const CHUNK_INDEX_LENGTH = 4;
 
 export const CHUNKS_LENGTH = 1024;
 
-export interface ChunkDat extends ReadonlyArray<Entry> {
+export interface ChunkEntries extends ReadonlyArray<Entry> {
   [index: number]: Entry;
 }
 
@@ -25,8 +31,8 @@ export interface Entry {
   length: number;
 }
 
-export function readChunkIndicies(data: Buffer): ChunkDat {
-  const chunkIndicies: ChunkDat = Object.seal(Array.from<ChunkDat[number]>({ length: CHUNKS_LENGTH }));
+export function readChunkEntries(data: Buffer): ChunkEntries {
+  const chunkEntries: ChunkEntries = Object.seal(Array.from<ChunkEntries[number]>({ length: CHUNKS_LENGTH }));
 
   for (let i = CHUNK_INDICIES_OFFSET; i < SECTOR_LENGTH; i += CHUNK_INDEX_LENGTH){
     const index: number = i / CHUNK_INDEX_LENGTH;
@@ -34,8 +40,8 @@ export function readChunkIndicies(data: Buffer): ChunkDat {
     const offset: number = (data.readUInt32LE(i) >> 8) * SECTOR_LENGTH;
     const length: number = offset !== 0 ? data.readUInt32LE(offset) : 0;
     const entry: Uint8Array | null = length !== 0 ? data.subarray(offset,offset + length) : null;
-    chunkIndicies[index] = { data: entry, index, sectors, offset, length };
+    chunkEntries[index] = { data: entry, index, sectors, offset, length };
   }
 
-  return chunkIndicies;
+  return chunkEntries;
 }
