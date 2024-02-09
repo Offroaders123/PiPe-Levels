7c958033bdf2b5103be9ae520a7ebbb2f38cb315
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-08 17:03:38 -0800

    Chunk Parser: Chunk Indicies Breakout
    Now the logic for the indicies parsing is located in it's own standalone function.

--- src/chunks.ts
+++ src/chunks.ts
@@ -1,20 +1,31 @@
+export interface ChunksDat {}
+
+export async function readChunksDat(data: Buffer): Promise<ChunksDat> {
+  // console.log(data);
+
+  console.log(data.subarray(0,SECTOR_LENGTH));
+  const chunkIndices = readChunkIndicies(data);
+  console.log(chunkIndices.slice(0,20));
+}
+
 export const SECTOR_LENGTH = 4096;
 
 export const CHUNK_INDICIES_OFFSET = 0;
 export const CHUNK_INDEX_LENGTH = 4;
 
-export interface ChunksDat {}
+export interface ChunkIndex {
+  sectors: number;
+  offset: number;
+}
 
-export async function readChunksDat(data: Buffer): Promise<ChunksDat> {
-  // console.log(data);
-  const chunkIndex = data.subarray(0,4096);
-  console.log(chunkIndex);
+export function readChunkIndicies(data: Buffer): ChunkIndex[] {
+  const chunkIndicies: ChunkIndex[] = [];
 
-  // chunk indicies parsing
   for (let i = CHUNK_INDICIES_OFFSET; i < SECTOR_LENGTH; i += CHUNK_INDEX_LENGTH){
-    const sectors: number = chunkIndex.readUInt8(i) * SECTOR_LENGTH;
-    const offset: number = (chunkIndex.readUInt32LE(i) >> 8) * SECTOR_LENGTH;
-    console.log({ sectors, offset });
-    if (i > 18 * CHUNK_INDEX_LENGTH) break;
+    const sectors: number = data.readUInt8(i) * SECTOR_LENGTH;
+    const offset: number = (data.readUInt32LE(i) >> 8) * SECTOR_LENGTH;
+    chunkIndicies.push({ sectors, offset });
   }
+
+  return chunkIndicies;
 }
