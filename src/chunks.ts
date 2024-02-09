104f6b5ee2c6e66d61ad0085c7dceb39926c8149
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-08 17:01:06 -0800

    Chunk Parser: Region-like Organization
    Making things follow a similar pattern to how I set things up over in MCA-Buffer.

--- src/chunks.ts
+++ src/chunks.ts
@@ -1,3 +1,8 @@
+export const SECTOR_LENGTH = 4096;
+
+export const CHUNK_INDICIES_OFFSET = 0;
+export const CHUNK_INDEX_LENGTH = 4;
+
 export interface ChunksDat {}
 
 export async function readChunksDat(data: Buffer): Promise<ChunksDat> {
@@ -5,10 +10,11 @@ export async function readChunksDat(data: Buffer): Promise<ChunksDat> {
   const chunkIndex = data.subarray(0,4096);
   console.log(chunkIndex);
 
-  for (let i = 0; i < 4096; i += 4){
-    const sectors: number = chunkIndex.readUInt8(i);
-    const offset: number = chunkIndex.readUInt32LE(i) >> 8;
+  // chunk indicies parsing
+  for (let i = CHUNK_INDICIES_OFFSET; i < SECTOR_LENGTH; i += CHUNK_INDEX_LENGTH){
+    const sectors: number = chunkIndex.readUInt8(i) * SECTOR_LENGTH;
+    const offset: number = (chunkIndex.readUInt32LE(i) >> 8) * SECTOR_LENGTH;
     console.log({ sectors, offset });
-    if (i > 18 * 4) break;
+    if (i > 18 * CHUNK_INDEX_LENGTH) break;
   }
 }
