abd543c6b85d9f597d19993bfd717ad5c4ebaca3
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-12 16:47:37 -0800

    LevelDat Types + BedrockLevel Header!!!
    Just had an *aha* moment! The version number byte at the start of Bedrock's `level.dat` file is the same as the `StorageVersion` NBT key itself!
    So this is something that can be actively typed-out in terms of a discriminated union type as well!
    
    Essentially,
    ```ts
    if (level.StorageVersion.valueOf() === 3){
      level; // old
    } else {
      level; // new
    }
    ```
    
    I thought the only way to get that info was through the file header itself, but maybe we can instead derive that from the NBT itself! This is extra cool.
    So for NBT handling, maybe `bedrockLevel` can be a boolean instead! And it will instead only look for whether a `StorageVersion` key is present. And if one is not, then it can error or something.
    Then you only have a single definition (or, source of truth) for handling that.
    
    https://minecraft.wiki/w/Bedrock_Edition_level_format/History#NBT_Structure
    
    Cornbread Crumb, Kidzapunk

--- src/level.ts
+++ src/level.ts
@@ -1,6 +1,7 @@
 import { read } from "nbtify";
 
-import type { Format, NBTData } from "nbtify";
+import type { Format, NBTData, IntTag, LongTag, StringTag } from "nbtify";
+import type { Player } from "../Region-Types/src/bedrock/entity.js";
 
 export const LEVEL_DAT_FORMAT = {
   rootName: "",
@@ -9,9 +10,30 @@ export const LEVEL_DAT_FORMAT = {
   bedrockLevel: 3
 } as const satisfies Format;
 
-export interface LevelDat extends NBTData {}
+export interface LevelDat extends NBTData {
+  GameType: IntTag<GameMode>;
+  LastPlayed: LongTag;
+  LevelName: StringTag;
+  Platform: IntTag<Platform>;
+  Player: Player; // may need to be adjusted to only include types for this older version
+  RandomSeed: LongTag;
+  SizeOnDisk: LongTag;
+  SpawnX: IntTag;
+  SpawnY: IntTag;
+  SpawnZ: IntTag;
+  StorageVersion: IntTag<StorageVersion>;
+  Time: LongTag;
+  dayCycleStopTime: LongTag;
+  spawnMobs: IntTag<0 | 1>; // not ByteTag, yeah weird I know, maybe it was going to be to do with difficulty originally? :)
+}
 
-export async function readLevelDat(data: Buffer): Promise<NBTData> {
-  const levelDat: LevelDat = await read(data,LEVEL_DAT_FORMAT);
+export type GameMode = 0 | 1;
+
+export type Platform = 2;
+
+export type StorageVersion = 3; // wait, hold on just a minute!!
+
+export async function readLevelDat(data: Buffer): Promise<NBTData<LevelDat>> {
+  const levelDat: NBTData<LevelDat> = await read<LevelDat>(data,LEVEL_DAT_FORMAT);
   return levelDat;
 }
